401ececafdb55c844148b06e3bd7fea146284deb
0xpinus <purple.emerald2019@gmail.com>
2022-05-24 08:26:49 -0400

    update single staking deploy script

--- scripts/deploySingleStaking.ts
+++ scripts/deploySingleStaking.ts
@@ -1,30 +1,29 @@
 import { ethers } from "hardhat";
 import { SingleStaking, Collection } from "../typechain";
-import { verifyContract, deployContract } from "../helper/deployer";
+import { verifyContract, deployContract, deployProxy } from "../helper/deployer";
 
 // Mumbai Staging Deployment
 async function main() {
-  const flameToken = "0x51aA4F3Ed9f11399Dd8B78b816a494bcA8704E11";
-  const treasury = "0xdAA1Ba6efCE73C16FddAB0423bC3F948cd381d1F";
-  const collection = "0x86A703193a3769f8Fb8180688bc35C3A282B7699";
+  const flameToken = "0x22e3f02f86Bc8eA0D73718A2AE8851854e62adc5";
+  const treasury = "0x000000000000000000000000000000000000dEaD";
+  const collection = "0x50B88955C82A6768a78Ad30b02af345F61ff3986";
 
-  const staking = <SingleStaking>await deployContract("SingleStaking");
-
-  await staking.initialize(flameToken, collection, treasury);
-
+  const staking = <SingleStaking>await deployProxy("SingleStaking", flameToken, collection, treasury);
+  
   const day = 3600 * 24;
 
   await staking.addTierInfo({
     apy: 0,
     power: 100,
-    penalty: 50,
+    penalty: 100,
     lockPeriod: 30 * day,
     fullPenaltyCliff: 0,
     penaltyMode: 0,
     isActive: true,
   });
+  console.log("tier 1 added");
   await staking.addTierInfo({
-    apy: 9,
+    apy: 6,
     power: 140,
     penalty: 40,
     lockPeriod: 180 * day,
@@ -32,26 +31,31 @@ async function main() {
     penaltyMode: 0,
     isActive: true,
   });
+  console.log("tier 2 added");
   await staking.addTierInfo({
-    apy: 15,
-    power: 190,
+    apy: 13,
+    power: 180,
     penalty: 35,
     lockPeriod: 365 * day,
     fullPenaltyCliff: 30 * day,
     penaltyMode: 1,
     isActive: true,
   });
+  console.log("tier 3 added");
   await staking.addTierInfo({
-    apy: 25,
-    power: 260,
+    apy: 23,
+    power: 250,
     penalty: 30,
     lockPeriod: 3 * 365 * day,
     fullPenaltyCliff: 90 * day,
     penaltyMode: 1,
     isActive: true,
   });
+  console.log("tier 4 added");
 
   console.log(staking.address);
+
+  await staking.transferOwnership("0xFa14B2103f0b590fD5d76624A5972061828Ba2e2");
 
   await verifyContract(staking.address);
 }
