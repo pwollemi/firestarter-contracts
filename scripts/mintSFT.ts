c829bf48f1ec9de6ca26c89b5ab041ed4b571893
0xlook <47668701+0xlook@users.noreply.github.com>
2022-05-31 19:41:41 +1000

    Update the sft mint script

--- scripts/mintSFT.ts
+++ scripts/mintSFT.ts
@@ -12,6 +12,7 @@ interface CSVData {
 async function main() {
   const firestarterSftAddress = "";
   const csvPath = ""; // ./scripts/data.csv
+  const checkMinted = true;
   const splits = 200;
 
   const firestarterSft = <FirestarterSft>(
@@ -48,11 +49,34 @@ async function main() {
     }
   }
 
-  for (let i = 0; i < csvData.length; i += splits) {
-    const end = csvData.length > i + splits ? i + splits : csvData.length;
-    const users = csvData.slice(i, end).map((el) => el.address);
-    const amounts = csvData.slice(i, end).map((el) => el.amount);
-    const unsets = csvData.slice(i, end).map((el) => el.unset);
+  let dataToMint: CSVData[] = [];
+
+  if (!checkMinted) {
+    dataToMint = checkMinted;
+  } else {
+    for (let i = 0; i < csvData.length; i++) {
+      const eventFilter = firestarterSft.filters.Transfer(
+        "0x0000000000000000000000000000000000000000",
+        csvData[i].address,
+        null
+      );
+      const events = await firestarterSft.queryFilter(eventFilter);
+      if (events.length === 0) {
+        dataToMint.push(csvData[i]);
+      } else {
+        const block = await events[0].getBlock();
+        if (Date.now() / 1000 - block.timestamp > 3600 * 8) {
+          dataToMint.push(csvData[i]);
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < dataToMint.length; i += splits) {
+    const end = dataToMint.length > i + splits ? i + splits : dataToMint.length;
+    const users = dataToMint.slice(i, end).map((el) => el.address);
+    const amounts = dataToMint.slice(i, end).map((el) => el.amount);
+    const unsets = dataToMint.slice(i, end).map((el) => el.unset);
 
     console.log(`minting from ${i} to ${end}`);
     await firestarterSft.batchMint(users, amounts, unsets);
